990497aa011028033f946ffd6459f894ef18a213
Matheus Gomes <zuza-cs@hotmail.com>
2015-02-23 03:18:42 -0300

    adicionando comentarios

--- autoload.php
+++ autoload.php
@@ -1,9 +1,11 @@
 <?php
+// Configurações de conexão com o banco de dados
 define("HOST", "localhost");
 define("DBNAME", "cursopoo");
 define("USER", "root");
 define("PASS", "root");
 
+// Configurações do autoload
 define("CLASS_DIR", __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR);
 
 spl_autoload_register(function($class) {
